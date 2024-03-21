eaea766e55fd62fd72799d05bd070e7a2ebfccc5
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-03-21 12:49:34 +0300

    seperate components and delete old codes

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>This is the main layout</h1>
         {children}
       </body>
     </html>
