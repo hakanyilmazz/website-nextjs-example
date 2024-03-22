e648c58f817d68c1aed23801472355e715606c96
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-03-22 11:32:14 +0300

    Add navbar + loading + error pages

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+const Links = () => {
+    const links = [
+        {
+            title: "Homepage",
+            path: "/"
+        },
+        {
+            title: "About",
+            path: "/about"
+        },
+        {
+            title: "Contact",
+            path: "/contact"
+        },
+        {
+            title: "Blog",
+            path: "/blog"
+        }
+    ];
+
+    return (
+        <div>
+            {
+                links.map((link) => {
+                    return <Link key={link.title} href={link.path}>{link.title}</Link>;
+                })
+            }
+        </div>
+    );
+}
+
+export default Links;
